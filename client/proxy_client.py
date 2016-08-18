8dab580b823e6dd474c0350b3f76d7e312677a63
icemoon1987 <panwenhai1987@163.com>
2016-08-18 17:38:56 +0800

    add some comment, remove unused code

--- client/proxy_client.py
+++ client/proxy_client.py
@@ -172,152 +172,16 @@ class proxy_client(object):
 	return
 
 
-    def send_proxy_request(self, proxy_request, proxy_server):
-
-	response = self.http_handler.request(proxy_request['method'], proxy_server, proxy_request['proxy_payload'], {'Content-Length':proxy_request['proxy_payload_len']}, crlf=proxy_request['need_crlf'])
-
-	response.app_status = response.status
-
-	if 'x-status' in response.msg:
-	    response.status = int(response.msg['x-status'])
-	    del response.msg['x-status']
-	if 'status' in response.msg:
-	    response.status = int(response.msg['status'])
-	    del response['status']
-
-	return response
-
-
-    def compose_proxy_request(self, user_request):
-
-	proxy_request = {}
-	if 'Range' in user_request['headers']:
-	    m = re.search('bytes=(\d+)-', user_request['headers']['Range'])
-	    start = int(m.group(1) if m else 0)
-	    user_request['headers']['Range'] = 'bytes=%d-%d' % (start, start + cfg.AUTORANGE_MAXSIZE-1)
-	    self.logger.info('autorange range=%r match url=%r', user_request['headers']['Range'], user_request['path'])
-
-	elif user_request['host'].endswith(cfg.AUTORANGE_HOSTS_TAIL):
-	    try:
-		pattern = (p for p in cfg.AUTORANGE_HOSTS if user_request['host'].endswith(p) or fnmatch.fnmatch(user_request['host'], p)).next()
-		self.logger.debug('autorange pattern=%r match url=%r', pattern, user_request['path'])
-		m = re.search('bytes=(\d+)-', user_request['headers'].get('Range', ''))
-		start = int(m.group(1) if m else 0)
-		user_request['headers']['Range'] = 'bytes=%d-%d' % (start, start + cfg.AUTORANGE_MAXSIZE-1)
-	    except StopIteration:
-		pass
-			    
-	# Compress the payload if necessary
-	if user_request['payload']:
-	    if len(user_request['payload']) < 10 * 1024 * 1024 and 'Content-Encoding' not in user_request['headers']:
-		zpayload = zlib.compress(user_request['payload'])[2:-4]
-		if len(zpayload) < len(user_request['payload']):
-		    user_request['payload'] = zpayload
-		    user_request['headers']['Content-Encoding'] = 'deflate'
-	    user_request['headers']['Content-Length'] = str(len(user_request['payload']))
-
-	# Warp the user request into proxy request
-	skip_headers = self.http_handler.skip_headers
-	metadata = 'User-Method:%s\nUser-Url:%s\n%s\n' % (user_request['method'], user_request['path'], '\n'.join('%s:%s'%(k,v) for k,v in user_request['headers'].iteritems() if k not in skip_headers))
-	metadata = zlib.compress(metadata)[2:-4]
-	proxy_payload = '%s%s%s' % (struct.pack('!h', len(metadata)), metadata, user_request['payload'])
-	need_crlf = 0
-	proxy_request['method'] = "POST"
-	proxy_request['proxy_payload'] = proxy_payload
-	proxy_request['proxy_payload_len'] = len(proxy_payload)
-        proxy_request['need_crlf'] = need_crlf
-
-	return proxy_request
-
-
-    def parse_proxy_response(self, response, sock):
-	result = ""
-	wfile = sock.makefile('wb', 0)
-	if 'Set-Cookie' in response.msg:
-	    response.msg['Set-Cookie'] = re.sub(', ([^ =]+(?:=|$))', '\\r\\nSet-Cookie: \\1', response.msg['Set-Cookie'])
-
-	wfile.write('HTTP/1.1 %s\r\n%s\r\n' % (response.status, ''.join('%s: %s\r\n' % (k.title(), v) for k, v in response.getheaders() if k != 'transfer-encoding')))
-
-	while 1:
-            data = response.read(8192)
-	    if not data:
-		break
-	    wfile.write(data)
-
-	response.close()
-
-	return result
-
-    def send_proxy_result(self, proxy_result, sock):
-        wfile = sock.makefile('wb', 0)
-	self.logger.info("proxy_result_len: " + str(len(proxy_result)))
-	wfile.write(proxy_result)
-	wfile.close()
-	sock.close()
-
-	return
-
-
-    def parse_user_request(self, sock):
-	result = {}
-	''' parse http parameters '''
-	rfile = sock.makefile('rb', __bufsize__)
-	method, path, version, headers = self.http_handler.parse_request(rfile)
-	''' handle CONNECT requests '''
-	__realsock = None
-	__realrfile = None
-
-	if method == 'CONNECT':
-	    host, _, port = path.rpartition(':')
-	    port = int(port)
-	    keyfile, certfile = CertUtil.get_cert(host)
-	    sock.sendall('HTTP/1.1 200 OK\r\n\r\n')
-	    __realsock = sock
-	    __realrfile = rfile
-
-	    try:
-	        sock = ssl.wrap_socket(__realsock, certfile=certfile, keyfile=keyfile, server_side=True)
-	    except Exception as e:
-		__realrfile.close()
-		__realsock.close()
-		return
-
-	    rfile = sock.makefile('rb', __bufsize__)
-	    try:
-		method, path, version, headers = self.http_handler.parse_request(rfile)
-	    except (EOFError, socket.error) as e:
-		if e[0] in ('empty line', 10053, errno.EPIPE):
-		    return rfile.close()
-		raise
-	    if path[0] == '/' and host:
-		path = 'https://%s%s' % (headers['Host'], path)
-	else:
-	    host = headers.get('Host', '')
-	    if path[0] == '/' and host:
-		path = 'http://%s%s' % (host, path)
-
-	content_length = int(headers.get('Content-Length', 0))
-	payload = rfile.read(content_length) if content_length else ''
-	result["method"] = method
-	result["path"] = path
-	result["version"] = version
-	result["headers"] = headers
-	result["host"] = host
-	result["content_length"] = content_length
-	result["payload"] = payload
-
-	return result
-
-
     ##
-    # @brief    handler for user browser requests
+    # @brief    Handler for user browser requests
+    #           For CONNECT request, this function will handle two requests: the CONNECT request and the following request.
+    #           For other request, this function will handle only one request.
     #
     # @param    sock: socket for this request
-    # @param    address: ip address of this request
+    # @param    address: (ip address, port) for client
     #
     # @return   
     def paasproxy_handler(self, sock, address):
-
 	http = self.http_handler
 
 	# Get user request url and parse it
@@ -329,25 +193,33 @@ class proxy_client(object):
 		return rfile.close()
 	    raise
 
-	# Uncomment this line if you need to fake browser.
+	# Change user-agent in header, if you need to fake browser 
 	#headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36'
 	remote_addr, remote_port = address
+
+        # Socket and rfilt for CONNECT request
 	__realsock = None
 	__realrfile = None
 
-	# For CONNECT request, establish a fake connection.
+	# For CONNECT request, establish the connection.
 	if method == 'CONNECT':
 	    host, _, port = path.rpartition(':')
 	    port = int(port)
+
+	    logging.info('%s:%s "CONNECT %s:%d HTTP/1.1" - -' % (address[0], address[1], host, port))
+
+            # Get certificate from target host
 	    keyfile, certfile = CertUtil.get_cert(host)
-	    logging.info('CONNECT %s:%s "%s:%d HTTP/1.1" - -' % (address[0], address[1], host, port))
 
 	    # Tell the user browser, the connection is established.
 	    sock.sendall('HTTP/1.1 200 OK\r\n\r\n')
+
+            # The response will be send by the socket that sent the CONNECT connect
 	    __realsock = sock
 	    __realrfile = rfile
+
 	    try:
-		# Warp the connection with fake certificate
+		# Wrap the connection with target host certificate
 		sock = ssl.wrap_socket(__realsock, certfile=certfile, keyfile=keyfile, server_side=True)
 	    except Exception as e:
 		logging.exception('ssl.wrap_socket(__realsock=%r) failed: %s', __realsock, e)
@@ -355,7 +227,7 @@ class proxy_client(object):
 		__realsock.close()
 		return
 
-	    # Get user browser's https request
+	    # Get user browser's next request
 	    rfile = sock.makefile('rb', __bufsize__)
 	    try:
 		method, path, version, headers = http.parse_request(rfile)
@@ -364,40 +236,49 @@ class proxy_client(object):
 		    return rfile.close()
 		raise
 
-	    if path[0] == '/' and host:
-		path = 'https://%s%s' % (headers['Host'], path)
-
+            # If the request uses CONNECT, it is a https request
+            proto = "https"
+        else:
+            proto = "http"
 
 	host = headers.get('Host', '')
 
+        # If path is not complete, compose one.
 	if path[0] == '/' and host:
-	    path = 'http://%s%s' % (host, path)
+	    path = proto + '://%s%s' % (host, path)
+
 	try:
 	    try:
-		# Wrap the user browser's request into a POST request, send the POST request to proxy_server
+	        # Wrap the user browser's request into a POST request, send the POST request to proxy_server
+	        # Get the proxy_server's response, unwarp the content.
 		content_length = int(headers.get('Content-Length', 0))
 		payload = rfile.read(content_length) if content_length else ''
 		response = self.paas_urlfetch(method, path, headers, payload, cfg.PROXY_SERVER)
-		logging.info('%s:%s "PAAS %s %s HTTP/1.1" %s -', remote_addr, remote_port, method, path, response.status)
+		logging.info('%s:%s "%s %s HTTP/1.1" %s -', remote_addr, remote_port, method, path, response.status)
+
 	    except socket.error as e:
+                # TODO: why ignore some exception? These exception will send the response back to user browser
 		if e.reason[0] not in (11004, 10051, 10060, 'timed out', 10054):
 		    raise
 	    except Exception as e:
-		logging.exception('error: %s', e)
+		logging.exception('socket error: %s', e)
 		raise
 
+            # Bad request, TODO: what's this?
 	    if response.app_status in (400, 405):
 		http.crlf = 0
 
-	    # Get the proxy_server's response, unwarp the content.
+            # Send the result back to user browser
 	    wfile = sock.makefile('wb', 0)
 
+            # Format Set-Cookie field, TODO: why doing this?
 	    if 'Set-Cookie' in response.msg:
 		response.msg['Set-Cookie'] = re.sub(', ([^ =]+(?:=|$))', '\\r\\nSet-Cookie: \\1', response.msg['Set-Cookie'])
 
+            # Write the response head to socket. Do not set transfer-encoding, TODO: why doing this?
 	    wfile.write('HTTP/1.1 %s\r\n%s\r\n' % (response.status, ''.join('%s: %s\r\n' % (k.title(), v) for k, v in response.getheaders() if k != 'transfer-encoding')))
 
-	    # Send the real page back to user browser.
+	    # Write the response payload to socket
 	    while 1:
 	        data = response.read(8192)
 		if not data:
@@ -421,11 +302,22 @@ class proxy_client(object):
 
         return
 
-
+    ##
+    # @brief    Wrap the user browser's request into a POST request, send the POST request to proxy_server
+    #           Get the proxy_server's response, unwarp the content.
+    #
+    # @param    method:     request method(GET POST ...)
+    # @param    url:        requesting url
+    # @param    headers:    request headers
+    # @param    payload:    payload of the request
+    # @param    fetchserver:proxy server url
+    # @param    kwargs:     other params, there params will be send to proxy server in header
+    #
+    # @return   response object
     def paas_urlfetch(self, method, url, headers, payload, fetchserver, **kwargs):
-	# deflate = lambda x:zlib.compress(x)[2:-4]
 	http = self.http_handler
 
+        # Compress the payload if the payload is not encoding or compressed.
 	if payload:
 	    if len(payload) < 10 * 1024 * 1024 and 'Content-Encoding' not in headers:
 		zpayload = zlib.compress(payload)[2:-4]
@@ -434,11 +326,22 @@ class proxy_client(object):
 		    headers['Content-Encoding'] = 'deflate'
 	    headers['Content-Length'] = str(len(payload))
 
+        # TODO: why skip some headers?
 	skip_headers = http.skip_headers
+
+        # Wrap user request into payload
 	metadata = 'User-Method:%s\nUser-Url:%s\n%s\n%s\n' % (method, url, '\n'.join('User-%s:%s'%(k,v) for k,v in kwargs.iteritems() if v), '\n'.join('%s:%s'%(k,v) for k,v in headers.iteritems() if k not in skip_headers))
 	metadata = zlib.compress(metadata)[2:-4]
+
+        # TODO: why send packed metadata and metadata in the same time?
 	app_payload = '%s%s%s' % (struct.pack('!h', len(metadata)), metadata, payload)
+
+        # Send to proxy server. TODO: what is crlf?
 	response = http.request('POST', fetchserver, app_payload, {'Content-Length':len(app_payload)}, crlf=0)
+
+        # The status of the proxy server's response is stored in app_status.
+        # The status of the user's real request is stored in status.
+        # TODO: what is x-status?
 	response.app_status = response.status
 	if 'x-status' in response.msg:
 	    response.status = int(response.msg['x-status'])
